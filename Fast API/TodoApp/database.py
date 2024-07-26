06f133a5aecfc6336aef0cfa0b7ad7751135c9c1
khuzaima-ocs <khuzaima.ahmed@ocloudsolutions.net>
2024-07-26 00:50:32 -0700

    Todo App with MySQL & Postgres

--- Fast API/TodoApp/database.py
+++ Fast API/TodoApp/database.py
@@ -2,11 +2,23 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DB_URL = "sqlite:///./todos.db"
+# DB = 'sqlite'
+DB = 'mysql'
+# DB = 'postgres'
 
-engine = create_engine(
-    DB_URL ,connect_args={"check_same_thread": False}
-)
+engine = None
+if DB == 'sqlite':
+    engine = create_engine(
+        "sqlite:///./todos.db" ,connect_args={"check_same_thread": False}
+    )
+elif DB == 'mysql':
+    engine = create_engine(
+        "mysql+pymysql://root:mysql@localhost:3306/todoApp"
+    )
+elif DB == 'postgres':
+    engine = create_engine(
+        "postgresql://postgres:postgres@localhost:5432/TodoAppDatabase"
+    )
 
 sessionLocal = sessionmaker(autoflush=False, bind=engine)
 
